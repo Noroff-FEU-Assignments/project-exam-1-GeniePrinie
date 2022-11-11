3a1ac891b9a3ac3a66d0cc3c6bc3b9646ccbbefa
GeniePrinie <geniexpn@gmail.com>
2022-11-11 15:50:05 +0100

    recipes page call api

---
+++ js/global/api.js
@@ -0,0 +1,17 @@
+async function fetchAllRecipes() {
+  const url =
+    "https://genieprinyanut.com/Noroff/living-organically-api/wp-json/wp/v2/posts?per_page=100";
+  const response = await fetch(url);
+  const recipes = await response.json();
+  return recipes;
+}
+
+async function fetchSingleRecipe(id) {
+  const url =
+    "https://genieprinyanut.com/Noroff/living-organically-api/wp-json/wp/v2/posts/" +
+    id +
+    "/?_embed=wp:term";
+  const response = await fetch(url);
+  const recipe = await response.json();
+  return recipe;
+}
--- js/recipes.js
+++ js/recipes.js
@@ -1,38 +1,31 @@
 const recipesContainer = document.querySelector(".recipes");
 recipesContainer.innerHTML = "";
 
-const url =
-  "https://genieprinyanut.com/Noroff/living-organically-api/wp-json/wp/v2/posts?per_page=100";
-
-async function fetchrecipe() {
-  try {
-    const response = await fetch(url);
-    const recipes = await response.json();
-
+try {
+  fetchAllRecipes().then((recipes) => {
     for (let i = 0; i < recipes.length; i++) {
-      let link = recipes[i].link;
+      let id = recipes[i].id;
+      let link = "/html/specificrecipe.html?id=" + id;
       let name = recipes[i].title.rendered;
       let image = recipes[i].better_featured_image.source_url;
       let description = recipes[i].content.rendered;
-      recipesContainer.innerHTML += `<article class="recipe">
-            <a href="${link}">
-             <img src="${image}" alt="${name}" />
-              <div class="recipe-content">
-                <h2>${name}</h2>
-                <p>
-                ${description}
-                </p>
-              </div>
-            </a>
-          </article>`;
+      createRecipeItem(link, name, image, description);
     }
-  } catch (error) {
-    recipesContainer.innerHTML = message("error", error);
-  }
+  });
+} catch (error) {
+  recipesContainer.innerHTML = `<div>${error}</div>`;
 }
 
-function message(messageType = "success", message = "") {
-  return `<div class="alert ${messageType}">${message}</div>`;
+function createRecipeItem(link, name, image, description) {
+  recipesContainer.innerHTML += `<article class="recipe">
+  <a href="${link}">
+   <img src="${image}" alt="${name}" />
+    <div class="recipe-content">
+      <h2>${name}</h2>
+      <p>
+      ${description}
+      </p>
+    </div>
+  </a>
+</article>`;
 }
-
-fetchrecipe();
